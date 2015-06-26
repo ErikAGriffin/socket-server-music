3c7a9ac20b7d754fe6c630dc01500d9c83a3fe33
Erik Griffin <griffin.erik@gmail.com>
2015-06-26 17:21:01 +0200

    Calling that method for every message..

--- server.rb
+++ server.rb
@@ -3,6 +3,11 @@ require 'multi_json'
 
 EM.run do
 
+    # Servers that close should not imediately be "deleted",
+    # so a client may push a song to a server whos browser accidentally closed
+    # and will reopen.
+
+
   def load_json(string)
     begin
       message = MultiJson.load(string, symbolize_keys: true)
@@ -13,26 +18,27 @@ EM.run do
   end
 
   def add_socket(message,socket)
+
+    # Must do server side error handling if a fony hostname gets sent in here.
+
+    # This is stupid that I must call this function on EVERY message send
+    # Is there not a way to add a socket to the appropriate hash location
+    # using only the handshake, AKA only when it is opened?
+
     host = message[:host]
-    # I am not particularly happy with this setup.
-    return if @servers.include?(socket) || (@clients[host] && @clients[host].include?(socket))
-    puts "----*" * 6
-    puts "I'm adding a socket #{message[:server] ? 'server' : 'client'}!"
-    if message[:server]
-      @servers[host] = socket
-      @clients[host] = []
+
+    if message[:server] && (!@sockets[host] || !@sockets[host][:server])
+      @sockets[host] = {server: socket}
     elsif !message[:server]
-      @clients[host] << socket
+      client_id = message[:id]
+      !@sockets[host][client_id] ? @sockets[host][client_id] = socket : socket
     end
+
   end
 
   puts "Event Machine running..."
 
-  @servers = {}
-  @clients = {}
-  # FINALLY an idea for different architecture.
-  # May have a single @sockets hash
-  # {HOSTNAME: {server:<serversocket>,clientID:<socket>, clientID:<socket>}
+  @sockets = {}
 
 #   Hash.new {|value, key| value[key.to_s] if Symbol === key }
 
@@ -47,15 +53,8 @@ EM.run do
     ws.onmessage do |data|
       message = load_json(data)
       p message
-      # Need server side protection, connecting to server that does not exist.
-      # Clients that leave can have their sockets deleted from array and reopened
-      # (What happens if you try to acces a closed socket in the Array?)
-      # However servers that close should not imediately be "deleted",
-      # so a client may push a song to a server whos browser accidentally closed
-      # and will reopen.
       add_socket(message,ws)
-      puts @servers
-      puts @clients
+      puts @sockets
     end
 
     ws.onclose do
