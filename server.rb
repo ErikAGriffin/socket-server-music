3ceba834b97d4b835d0a1073462b438f85a9cc15
Erik Griffin <griffin.erik@gmail.com>
2015-06-26 16:42:36 +0200

    Playing with socket architecture

--- server.rb
+++ server.rb
@@ -1,21 +1,39 @@
 require 'em-websocket'
 require 'multi_json'
 
-def load_json(string)
-  begin
-    message = MultiJson.load(string, symbolize_keys: true)
-  rescue MultiJson::ParseError => exception
-    message = {error: exception.cause, data:exception.data}
-  end
-  message
-end
-
 EM.run do
+
+  def load_json(string)
+    begin
+      message = MultiJson.load(string, symbolize_keys: true)
+    rescue MultiJson::ParseError => exception
+      message = {error: exception.cause, data:exception.data}
+    end
+    message
+  end
+
+  def add_socket(message,socket)
+    host = message[:host]
+    # I am not particularly happy with this setup.
+    return if @servers.include?(socket) || (@clients[host] && @clients[host].include?(socket))
+    puts "----*" * 6
+    puts "I'm adding a socket #{message[:server] ? 'server' : 'client'}!"
+    if message[:server]
+      @servers[host] = socket
+      @clients[host] = []
+    elsif !message[:server]
+      @clients[host] << socket
+    end
+  end
 
   puts "Event Machine running..."
 
   @servers = {}
   @clients = {}
+  # FINALLY an idea for different architecture.
+  # May have a single @sockets hash
+  # {HOSTNAME: {server:<serversocket>,clientID:<socket>, clientID:<socket>}
+
 #   Hash.new {|value, key| value[key.to_s] if Symbol === key }
 
   # hmm.. How to set this up for the heroku server
@@ -23,24 +41,21 @@ EM.run do
 
     ws.onopen do |handshake|
       puts "WebSocket has opened!"
-
-      # Why here over outside ?
-#       ws.onmessage do |message|
-#         client = load_json(message)
-#         p client
-#       end
     end
-
 
     # This is not called if it is defined inside onopen.
     ws.onmessage do |data|
       message = load_json(data)
-      if message[:type] = "server"
-        @servers[message[:name]] = ws if !@servers[message[:name]]
-      else
-        @clients[message[:name]] = ws if !@clients[message[:name]]
-      end
-
+      p message
+      # Need server side protection, connecting to server that does not exist.
+      # Clients that leave can have their sockets deleted from array and reopened
+      # (What happens if you try to acces a closed socket in the Array?)
+      # However servers that close should not imediately be "deleted",
+      # so a client may push a song to a server whos browser accidentally closed
+      # and will reopen.
+      add_socket(message,ws)
+      puts @servers
+      puts @clients
     end
 
     ws.onclose do
